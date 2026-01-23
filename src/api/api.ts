d443341b1966f20c341c60567b4bde52563cac87
winsonLuong <luonggiahao2005@gmail.com>
2026-01-23 19:17:20 +0700

    fix api sign up

--- src/api/api.ts
+++ src/api/api.ts
@@ -9,6 +9,10 @@ const api = axios.create({
     "Content-Type": "application/json",
   },
 });
+export interface SignupPayload {
+  username: string;
+  password: string;
+}
 export const login = async (
   username: string,
   password: string,
@@ -25,3 +29,9 @@ export async function getPosts(): Promise<Post[]> {
   const { data } = await api.get<Post[]>("/post");
   return data;
 }
+
+export const signup = async (payload: SignupPayload): Promise<User> => {
+  // Directly create new user (POST)
+  const { data: user } = await api.post<User>("/user", payload);
+  return user;
+};
